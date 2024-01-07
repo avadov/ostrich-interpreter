b4a0fe9aab660bc117c4b4c5d9cb3cb3cad5d007
Konstantin Avadov <konstantin.avadov@yahoo.com>
2024-01-07 12:25:36 +0300

    Add readIdentifier() function

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,12 +9,18 @@ type Lexer struct {
 	ch byte // current char
 }
 
-func New(input string) *Lexer {
+/*
+ * Initialize a new lexer
+ */
+func NewLexer(input string) *Lexer {
 	l := &Lexer{input: input}
 	l.readChar()
 	return l
 }
 
+/*
+ * Read the next character and advance the position in the input string
+ */
 func (l *Lexer) readChar() {
 	// TODO: Unicode support here
 	if l.readPosition >= len(l.input) {
@@ -26,6 +32,9 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+/*
+ * Return a token depending on the current character
+ */
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
@@ -49,6 +58,13 @@ func (l *Lexer) NextToken() token.Token {
 		case 0:
 			tok.Literal = ""
 			tok.Type = token.EOF
+		default:
+			if isLetter(l.ch) {
+				tok.Literal = l.readIdentifier()
+				return tok 
+			} else {
+				tok = newToken(token.ILLEGAL, l.ch)
+			}
 	}
 
 	l.readChar()
@@ -57,4 +73,22 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+/*
+ * Read and return the current identifier and advance lexer's position until
+ * it encounters a non-letter-character
+ */
+func (l *Lexer) readIdentifier() string {
+	start_position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[start_position, l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 
+		'A' <= ch && ch <= 'Z' ||
+		ch == '_'
 }
