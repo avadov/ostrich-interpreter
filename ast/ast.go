194a9bcae92317238ad8c2153dd264c67b07a25d
Konstantin Avadov <konstantin.avadov@yahoo.com>
2024-02-04 10:33:01 +0300

    Add identifier token

--- ast/ast.go
+++ ast/ast.go
@@ -29,6 +29,7 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+
 type LetStatement struct {
 	Token token.Token  // the token.LET token
 	Name *Identifier
@@ -39,4 +40,16 @@ func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+
+type Identifier struct {
+	Token token.Token  // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral string {
+	return i.Token.Literal
 }
