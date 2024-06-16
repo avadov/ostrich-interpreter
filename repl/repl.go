45cac994c91f79d38fdfef26420e314ea99cba82
Konstantin Avadov <konstantin.avadov@yahoo.com>
2024-06-16 19:35:23 +0300

    Create internal representations for integer, boolean and null values

--- repl/repl.go
+++ repl/repl.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"ostrich-interpreter/token"
 	"ostrich-interpreter/lexer"
+	"ostrich-interpreter/parser"
 )
 
 const PROMPT = "> "
@@ -22,10 +22,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.NewLexer(line)
+		p := parser.NewParser(l)
 
-		tok := l.NextToken()
-		for ; tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Warning! Parser errors: \n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
