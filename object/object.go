b26a7e34ddd71e535f4363c5675b4dcba25cc725
Konstantin Avadov <konstantin.avadov@yahoo.com>
2024-06-23 17:47:02 +0300

    Error handling

--- object/object.go
+++ object/object.go
@@ -12,6 +12,7 @@ const (
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
 )
 
 type Object interface {
@@ -62,3 +63,14 @@ type ReturnValue struct {
 
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+////////////////////////////////////////
+
+type Error struct {
+	Message string
+}
+
+// TODO Add the line and column number of the error. Add stack trace.
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
